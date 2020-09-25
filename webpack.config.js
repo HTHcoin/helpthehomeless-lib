adcb2e5152f41e4c966fc1711771a8849fa9f657
Altcoin-Cash <altcoincash@protonmail.com>
2020-09-26 00:30:25 +0200

    Homelessed

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// TODO: Remove previous line and work through linting issues at next edit
+
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const commonJSConfig = {
+  entry: ['./index.js'],
+  module: {
+    rules: [],
+  },
+  target: 'web'
+};
+
+const rawConfig = Object.assign({}, commonJSConfig, {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'helpthehomeless-lib.js',
+    library: 'helpthehomeless',
+    libraryTarget: 'umd',
+  }
+})
+const uglifiedConfig = Object.assign({}, commonJSConfig, {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'helpthehomeless-lib.min.js',
+    library: 'helpthehomeless',
+    libraryTarget: 'umd',
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+})
+
+module.exports = [rawConfig, uglifiedConfig];
